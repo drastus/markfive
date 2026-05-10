679f69c97db984cdf79de227ce9c99a95c6b69a4
Michał Górny <zrchos@gmail.com>
2026-05-10 23:25:09 +0200

    Use rollup-plugin-dts

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import copy from '@guanghechen/rollup-plugin-copy';
 import typescript from '@rollup/plugin-typescript';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
+import {dts} from 'rollup-plugin-dts';
 
 export default [
 	{
@@ -10,10 +11,18 @@ export default [
 			format: 'es',
 			sourcemap: true,
 		},
-		plugins: [typescript({
-			declarationDir: 'lib',
-			declaration: true,
-		}), nodeResolve()],
+		plugins: [typescript(), nodeResolve()],
+	},
+	{
+		input: 'src/index.ts',
+		output: {
+			file: 'lib/index.d.ts',
+			format: 'es',
+		},
+		plugins: [
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+			dts(),
+		],
 	},
 	{
 		input: 'src/scripts/notes.ts',
